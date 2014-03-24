bc3bb85559f0951a6177d3ca4d9f0ad26e64332b
root <neginsky58@gmail.com>
2014-03-24 17:48:06 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Citii::Application.config.secret_key_base = '2bfa86d5eb73c6aed0324ebce5c4f11d7164f2536e78abce0e82581e619095d8bf559aec664f8941fc2f165bbc99bb1125ee758c9617be975189ec68e7d99efe'
